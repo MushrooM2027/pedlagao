47bd177e4710037cca82c248231ce9eea3f3bf36
MushrooM2027 <adityaprakash140103@gmail.com>
2025-05-01 17:05:10 +0530

    database deployed

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,11 +1,10 @@
 require('dotenv').config(); // <-- load .env file
 
 const { Sequelize } = require('sequelize');
-const dbPassword = process.env.DB_PASSWORD;
-const sequelize = new Sequelize('pedlagao', 'root', dbPassword, {
-  host: 'localhost',
+// const MYSQL_URL = process.env.MYSQL_URL;
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
   dialect: 'mysql',
-  timezone: '+05:30'
+  timezone: '+05:30',
 });
 
 module.exports = sequelize;
